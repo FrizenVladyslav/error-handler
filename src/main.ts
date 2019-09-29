ab13d695283ed47aa651ef133fd8109f3bcf759e
FrizenVladyslav <FrizenVladyslav>
2019-09-29 22:20:54 +0300

    cors helmet

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors();
 
   const options = new DocumentBuilder()
     .setTitle('Error handler')
